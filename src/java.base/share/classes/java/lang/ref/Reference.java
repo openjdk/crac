AntonKozlov/cracui

Ensure empty Reference Handler and Cleaners queues

src/java.base/share/classes/java/lang/ref/Reference.java
@@ -25,6 +25,9 @@
 
 package java.lang.ref;
 
+import jdk.crac.Context;
+import jdk.crac.Resource;
+import jdk.internal.crac.JDKResource;
 import jdk.internal.vm.annotation.ForceInline;
 import jdk.internal.vm.annotation.IntrinsicCandidate;
 import jdk.internal.access.JavaLangRefAccess;
@@ -245,6 +248,8 @@ public abstract class Reference<T> {
     private static final Object processPendingLock = new Object();
     private static boolean processPendingActive = false;
 
+    private static JDKResource referenceHandlerSyncer;
+
     private static void processPendingReferences() {
         // Only the singleton reference processing thread calls
         // waitForReferencePendingList() and getAndClearReferencePendingList().
@@ -327,6 +332,24 @@ public abstract class Reference<T> {
                 Finalizer.runFinalization();
             }
         });
+
+        referenceHandlerSyncer = new JDKResource() {
+            @Override
+            public Priority getPriority() {
+                return Priority.REFERENCE_HANDLER;
+            }
+
+            @Override
+            public void beforeCheckpoint(Context<? extends Resource> context) throws Exception {
+                System.gc();
+                while (waitForReferenceProcessing());
+            }
+
+            @Override
+            public void afterRestore(Context<? extends Resource> context) throws Exception {
+            }
+        };
+        jdk.internal.crac.Core.getJDKContext().register(referenceHandlerSyncer);
     }
 
     /* -- Referent accessor and setters -- */

src/java.base/share/classes/jdk/internal/ref/CleanerImpl.java
@@ -34,13 +34,16 @@ import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 
+import jdk.crac.Context;
+import jdk.crac.Resource;
+import jdk.internal.crac.JDKResource;
 import jdk.internal.misc.InnocuousThread;
 
 /**
  * CleanerImpl manages a set of object references and corresponding cleaning actions.
  * CleanerImpl provides the functionality of {@link java.lang.ref.Cleaner}.
  */
-public final class CleanerImpl implements Runnable {
+public final class CleanerImpl implements Runnable, JDKResource {
 
     /**
      * An object to access the CleanerImpl from a Cleaner; set by Cleaner init.
@@ -83,6 +86,7 @@ public final class CleanerImpl implements Runnable {
     public CleanerImpl() {
         queue = new ReferenceQueue<>();
         phantomCleanableList = new PhantomCleanableRef();
+        jdk.internal.crac.Core.getJDKContext().register(this);
     }
 
     /**
@@ -146,6 +150,22 @@ public final class CleanerImpl implements Runnable {
                 // (including interruption of cleanup thread)
             }
         }
+    }
+
+
+    @Override
+    public Priority getPriority() {
+        return Priority.CLEANER;
+    }
+
+    @Override
+    public void beforeCheckpoint(Context<? extends Resource> context) throws Exception {
+        queue.waitForWaiters(1);
+    }
+
+    @Override
+    public void afterRestore(Context<? extends Resource> context) throws Exception {
+
     }
 
     /**

openjdk/crac

src/java.base/share/classes/java/lang/ref/Reference.java
@@ -25,6 +25,9 @@
 
 package java.lang.ref;
 
+import jdk.crac.Context;
+import jdk.crac.Resource;
+import jdk.internal.crac.JDKResource;
 import jdk.internal.vm.annotation.ForceInline;
 import jdk.internal.vm.annotation.IntrinsicCandidate;
 import jdk.internal.access.JavaLangRefAccess;
@@ -245,6 +248,8 @@ public abstract class Reference<T> {
     private static final Object processPendingLock = new Object();
     private static boolean processPendingActive = false;
 
+    private static JDKResource referenceHandlerResource;
+
     private static void processPendingReferences() {
         // Only the singleton reference processing thread calls
         // waitForReferencePendingList() and getAndClearReferencePendingList().
@@ -327,6 +332,25 @@ public abstract class Reference<T> {
                 Finalizer.runFinalization();
             }
         });
+
+        referenceHandlerResource = new JDKResource() {
+            @Override
+            public Priority getPriority() {
+                return Priority.REFERENCE_HANDLER;
+            }
+
+            @Override
+            public void beforeCheckpoint(Context<? extends Resource> context) throws Exception {
+                System.gc();
+                // TODO ensure GC done processing all References
+                while (waitForReferenceProcessing());
+            }
+
+            @Override
+            public void afterRestore(Context<? extends Resource> context) throws Exception {
+            }
+        };
+        jdk.internal.crac.Core.getJDKContext().register(referenceHandlerResource);
     }
 
     /* -- Referent accessor and setters -- */

src/java.base/share/classes/jdk/internal/ref/CleanerImpl.java
@@ -34,13 +34,16 @@ import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 
+import jdk.crac.Context;
+import jdk.crac.Resource;
+import jdk.internal.crac.JDKResource;
 import jdk.internal.misc.InnocuousThread;
 
 /**
  * CleanerImpl manages a set of object references and corresponding cleaning actions.
  * CleanerImpl provides the functionality of {@link java.lang.ref.Cleaner}.
  */
-public final class CleanerImpl implements Runnable {
+public final class CleanerImpl implements Runnable, JDKResource {
 
     /**
      * An object to access the CleanerImpl from a Cleaner; set by Cleaner init.
@@ -83,6 +86,7 @@ public final class CleanerImpl implements Runnable {
     public CleanerImpl() {
         queue = new ReferenceQueue<>();
         phantomCleanableList = new PhantomCleanableRef();
+        jdk.internal.crac.Core.getJDKContext().register(this);
     }
 
     /**
@@ -146,6 +150,20 @@ public final class CleanerImpl implements Runnable {
                 // (including interruption of cleanup thread)
             }
         }
+    }
+
+    @Override
+    public Priority getPriority() {
+        return Priority.CLEANERS;
+    }
+
+    @Override
+    public void beforeCheckpoint(Context<? extends Resource> context) throws Exception {
+        queue.waitForWaiters(1);
+    }
+
+    @Override
+    public void afterRestore(Context<? extends Resource> context) throws Exception {
     }
 
     /**
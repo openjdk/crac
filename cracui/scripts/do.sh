1cae049088290b50943540acfc8e9996709ffb3d
kznts9v-1lya <kuznetsov.ilya.alexandrovich@gmail.com>
2022-02-16 12:19:41 +0300

    Script path changes

--- cracui/scripts/do.sh
+++ cracui/scripts/do.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-bash configure --with-boot-jdk=/home/kznts9v_1lya/Education/CRaC_Java_UI/OpenJDK/jdk-14/ --with-jtreg=/home/kznts9v_1lya/Education/CRaC_Java_UI/OpenJDK/jdk/jtreg/
+bash configure --with-boot-jdk=/home/kznts9v_1lya/Education/CRaC_Java_UI/OpenJDK/jdk-17/ --with-jtreg=/home/kznts9v_1lya/Education/CRaC_Java_UI/OpenJDK/jdk/jtreg/
 make images
 cp -f ../../Criu/criu-dist/sbin/criu ./build/linux-x86_64-server-release/images/jdk/lib/
 sudo chown root:root build/linux-x86_64-server-release/images/jdk/lib/criu
--- cracui/scripts/get.sh
+++ cracui/scripts/get.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rm -rf ./jdk/
-mv -f build/linux-x86_64-server-release/images/jdk/ .
+rm -rf ../jdk-17/jdk/
+mv -f build/linux-x86_64-server-release/images/jdk/ ../jdk-17/

8cac85e93cdac16017741791e95f35daae4182d2
Anton Kozlov <akozlov@azul.com>
2022-12-09 13:47:23 +0200

    Update message

--- src/java.base/share/classes/jdk/internal/util/jar/PersistentJarFile.java
+++ src/java.base/share/classes/jdk/internal/util/jar/PersistentJarFile.java
@@ -45,7 +45,7 @@ public class PersistentJarFile extends JarFile implements JDKResource {
 
     @Override
     public void beforeCheckpoint(Context<? extends Resource> context) throws Exception {
-        logger.log(System.Logger.Level.INFO, "Assuming persistent " + this.getName());
+        logger.log(System.Logger.Level.INFO, this.getName() + " is recorded as always available on restore");
         SharedSecrets.getJavaUtilZipFileAccess().beforeCheckpoint(this);
     }
 

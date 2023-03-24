9359b6b00506fc9a2a470a6f7f165f50c35faf95
Radim Vansa <rvansa@azul.com>
2023-03-24 11:24:12 +0100

    Revert "Let ReseedTest use stdout rather than exit codes"
    This reverts commit 704f50a4ea9591ae1dd04aae7559492441ea4c2e.

--- test/jdk/jdk/crac/SecureRandom/ReseedTest.java
+++ test/jdk/jdk/crac/SecureRandom/ReseedTest.java
@@ -44,9 +44,8 @@ public class ReseedTest implements CracTest {
     public void test() throws Exception {
         CracBuilder builder = new CracBuilder();
         builder.doCheckpoint();
-        builder.captureOutput(true);
-        String e1 = builder.doRestore().outputAnalyzer().getStdout();
-        String e2 = builder.doRestore().outputAnalyzer().getStdout();
+        int e1 = builder.startRestore().waitFor();
+        int e2 = builder.startRestore().waitFor();
         if (reseed) {
             assertEquals(e1, e2);
         } else {
@@ -71,7 +70,7 @@ public class ReseedTest implements CracTest {
             throw new RuntimeException("Restore ERROR " + e);
         }
 
-        System.out.println(sr.nextInt());
-        System.exit(0);
+        int r = sr.nextInt(255);
+        System.exit(r);
     }
 }

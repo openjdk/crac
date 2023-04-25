22e75f7c49eed21a64d8c822fe87f67be54c9d84
Radim Vansa <rvansa@azul.com>
2023-04-25 09:28:34 +0200

    Don't use SwitchPoints

--- src/java.base/share/classes/jdk/crac/RCULock.java
+++ src/java.base/share/classes/jdk/crac/RCULock.java
@@ -6,7 +6,6 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import java.lang.invoke.*;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
@@ -15,12 +14,11 @@ import java.util.List;
 import java.util.TreeSet;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 /**
- * This class represents a RW-lock with extremely lightweight read-locking
+ * This class represents a non-reentrant RW-lock with extremely lightweight read-locking
  * and heavyweight write locking.
  * <p>
  * Readers should use the {@link #readLock()} and {@link #readUnlock()}; when
@@ -60,16 +58,12 @@ public class RCULock {
     private final ReentrantLock lock = new ReentrantLock(true);
     private final Condition beginCondition = lock.newCondition();
     private final Condition endCondition = lock.newCondition();
-    private boolean synchronize;
+    private volatile boolean synchronize;
     @SuppressWarnings("unused") // used in native code
     private long readerThreadsList;
     @SuppressWarnings("unused") // used in native code
     private long readCriticalMethods;
     private final TreeSet<String> methodsCopy;
-    private SwitchPoint lockSwitchPoint = new SwitchPoint();
-    private SwitchPoint unlockSwitchPoint = new SwitchPoint();
-    private volatile MethodHandle lockImpl;
-    private volatile MethodHandle unlockImpl;
 
     static {
         try {
@@ -139,7 +133,6 @@ public class RCULock {
      * @param readCriticalMethods List of signatures for methods invoked in the read-critical section.
      */
     public RCULock(String[] readCriticalMethods) {
-        initSwitchPoints();
         methodsCopy = new TreeSet<>(Arrays.asList(readCriticalMethods));
         //noinspection CapturingCleaner
         CleanerFactory.cleaner().register(this, this::destroy);
@@ -196,25 +189,6 @@ public class RCULock {
 
     private native void updateMethods(String[] methods);
 
-    private void initSwitchPoints() {
-        try {
-            MethodType voidType = MethodType.methodType(void.class);
-            MethodHandles.Lookup lookup = MethodHandles.lookup();
-            MethodHandle noop = lookup.findSpecial(RCULock.class, "noop", voidType, RCULock.class);
-            MethodHandle readLockImpl = lookup.findSpecial(RCULock.class, "readLockImpl", voidType, RCULock.class);
-            MethodHandle readUnlockImpl = lookup.findSpecial(RCULock.class, "readUnlockImpl", voidType, RCULock.class);
-            lockSwitchPoint = new SwitchPoint();
-            lockImpl = lockSwitchPoint.guardWithTest(noop, readLockImpl);
-            unlockSwitchPoint = new SwitchPoint();
-            unlockImpl = lockSwitchPoint.guardWithTest(noop, readUnlockImpl);
-        } catch (NoSuchMethodException | IllegalAccessException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void noop() {
-    }
-
     /**
      * Acquire the read-lock. This method <strong>must</strong> be called from
      * within the read-critical method declared when this lock is created.
@@ -226,29 +200,27 @@ public class RCULock {
      */
     public void readLock() {
         try {
-            lockImpl.invokeExact(this);
+            if (synchronize) {
+                lock.lock();
+                try {
+                    // In case that we have been caught in the critical section
+                    // before calling readLock() this thread was added to the list
+                    // but won't be removed as we will block.
+                    // After continuing in the critical section any subsequent
+                    // synchronizeBegin will catch us in again, so we don't need to
+                    // do anything.
+                    removeThread();
+                    beginCondition.signal();
+                    // The lock is released as we are waiting for beginCondition
+                    while (synchronize) {
+                        endCondition.awaitUninterruptibly();
+                    }
+                } finally {
+                    lock.unlock();
+                }
+            }
         } catch (Throwable e) {
             throw new RuntimeException(e);
-        }
-    }
-
-    private void readLockImpl() {
-        lock.lock();
-        try {
-            // In case that we have been caught in the critical section
-            // before calling readLock() this thread was added to the list
-            // but won't be removed as we will block.
-            // After continuing in the critical section any subsequent
-            // synchronizeBegin will catch us in again, so we don't need to
-            // do anything.
-            removeThread();
-            beginCondition.signal();
-            // The lock is released as we are waiting for beginCondition
-            while (synchronize) {
-                endCondition.awaitUninterruptibly();
-            }
-        } finally {
-            lock.unlock();
         }
     }
 
@@ -258,19 +230,17 @@ public class RCULock {
      */
     public void readUnlock() {
         try {
-            unlockImpl.invokeExact(this);
+            if (synchronize) {
+                lock.lock();
+                try {
+                    removeThread();
+                    beginCondition.signal();
+                } finally {
+                    lock.unlock();
+                }
+            }
         } catch (Throwable e) {
             throw new RuntimeException(e);
-        }
-    }
-
-    private void readUnlockImpl() {
-        lock.lock();
-        try {
-            removeThread();
-            beginCondition.signal();
-        } finally {
-            lock.unlock();
         }
     }
 
@@ -288,7 +258,6 @@ public class RCULock {
             throw new IllegalMonitorStateException("Concurrent synchronization or bug?");
         }
         synchronize = true;
-        SwitchPoint.invalidateAll(new SwitchPoint[]{ lockSwitchPoint, unlockSwitchPoint });
         synchronizeThreads();
         while (hasReaderThreads()) {
             beginCondition.awaitUninterruptibly();
@@ -308,7 +277,6 @@ public class RCULock {
         if (!lock.isHeldByCurrentThread()) {
             throw new IllegalMonitorStateException("The lock is not held by ");
         }
-        initSwitchPoints();
         synchronize = false;
         endCondition.signalAll();
         lock.unlock();

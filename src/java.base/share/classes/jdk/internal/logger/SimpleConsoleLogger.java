d9d9e48c59cd0cddd92f72ce7316d2bcc6288717
Radim Vansa <rvansa@azul.com>
2023-05-12 15:21:53 +0200

    Effectively revert previous commit: Initialize logger in <clinit>

--- src/java.base/share/classes/jdk/internal/logger/SimpleConsoleLogger.java
+++ src/java.base/share/classes/jdk/internal/logger/SimpleConsoleLogger.java
@@ -31,6 +31,7 @@ import java.io.StringWriter;
 import java.lang.StackWalker.StackFrame;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
+import java.text.MessageFormat;
 import java.time.ZonedDateTime;
 import java.util.Optional;
 import java.util.MissingResourceException;
@@ -57,9 +58,12 @@ public class SimpleConsoleLogger extends LoggerConfiguration
 
     static {
         // It is possible that CRaC would print first logging message after
-        // file descriptors handling; initialization of ZonedDateTime opens
-        // zone files, though. Initialize eagerly instead.
-        ZonedDateTime.now();
+        // JDKContext completes. Registering a CallSite as resource at this
+        // point would hang the checkpoint, therefore we perform all the
+        // initialization eagerly.
+        new SimpleConsoleLogger(null, false).getCallerInfo();
+        String.format("%tc",ZonedDateTime.now());
+        MessageFormat.format("{0} {1}", 0, "1");
     }
 
     static Level getDefaultLevel() {

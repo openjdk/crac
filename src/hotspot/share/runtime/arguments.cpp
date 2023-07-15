97ae86d6b4455b3a84c6c361b51cb524a504c1f5
Jan Kratochvil <jkratochvil@openjdk.org>
2023-07-15 12:50:50 +0200

    Add the exception message.

--- src/hotspot/share/runtime/arguments.cpp
+++ src/hotspot/share/runtime/arguments.cpp
@@ -3129,6 +3129,7 @@ jint Arguments::finalize_vm_init_args(bool patch_mod_javabase) {
   }
 
   if (CRaCCheckpointTo && UseZGC) {
+    # jdk.crac.impl.CheckpointOpenFileException: FD fd=3 type=regular path=/memfd:java_heap (deleted)
     jio_fprintf(defaultStream::output_stream(),
       "-XX:+UseZGC is currently unsupported for -XX:CRaCCheckpointTo, contact Azul for ZULU-13196.\n");
     return JNI_ERR;

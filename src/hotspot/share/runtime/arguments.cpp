39c2a2d444baa3ff9d0663ce963a88c6678a6cf6
Jan Kratochvil <jkratochvil@openjdk.org>
2023-07-15 12:47:53 +0200

    Implement: -XX:+UseZGC is currently unsupported for -XX:CRaCCheckpointTo

--- src/hotspot/share/runtime/arguments.cpp
+++ src/hotspot/share/runtime/arguments.cpp
@@ -3128,6 +3128,12 @@ jint Arguments::finalize_vm_init_args(bool patch_mod_javabase) {
     return JNI_ERR;
   }
 
+  if (CRaCCheckpointTo && UseZGC) {
+    jio_fprintf(defaultStream::output_stream(),
+      "-XX:+UseZGC is currently unsupported for -XX:CRaCCheckpointTo, contact Azul for ZULU-13196.\n");
+    return JNI_ERR;
+  }
+
   // This must be done after all arguments have been processed
   // and the container support has been initialized since AggressiveHeap
   // relies on the amount of total memory available.

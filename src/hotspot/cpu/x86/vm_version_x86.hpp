fe9af53e33265ddb8a2010065342e41a373c8251
Jan Kratochvil <jkratochvil@openjdk.org>
2023-08-15 06:20:48 +0800

    Remove an unused nonlibc_str_equals declaration.  - found by Anton Kozlov

--- src/hotspot/cpu/x86/vm_version_x86.hpp
+++ src/hotspot/cpu/x86/vm_version_x86.hpp
@@ -627,7 +627,6 @@ private:
   static void nonlibc_tty_print_uint64(uint64_t num);
   static void nonlibc_tty_print_uint64_comma_uint64(uint64_t num1, uint64_t num2);
   static void print_using_features_cr();
-  static bool nonlibc_str_equals(const char *a, const char *b);
   /*[[noreturn]]*/ static void missing_features(uint64_t features_missing, uint64_t glibc_features_missing);
 
   static uint64_t feature_flags();

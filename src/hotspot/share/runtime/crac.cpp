eb87a387020e837e0481661725ec8e8421e5c00b
Jan Kratochvil <jkratochvil@openjdk.org>
2023-09-04 16:53:19 +0000

    Ensure G1 GC is completed before creating a snapshot
    Reviewed-by: akozlov

--- src/hotspot/share/runtime/crac.cpp
+++ src/hotspot/share/runtime/crac.cpp
@@ -420,6 +420,7 @@ Handle crac::checkpoint(jarray fd_arr, jobjectArray obj_arr, bool dry_run, jlong
   Universe::heap()->set_cleanup_unused(true);
   Universe::heap()->collect(GCCause::_full_gc_alot);
   Universe::heap()->set_cleanup_unused(false);
+  Universe::heap()->finish_collection();
 
   VM_Crac cr(fd_arr, obj_arr, dry_run, (bufferedStream*)jcmd_stream);
   {

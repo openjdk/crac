4c89bcbffb61bd5a12b0d739755cb009509bb22e
Jan Kratochvil <jkratochvil@openjdk.org>
2023-09-05 16:17:35 +0800

    Keep GLOBAL_CONTEXT_IMPL_PROP, rename impl_name to implName.  - suggested by Anton Kozlov

--- src/java.base/share/classes/jdk/crac/impl/GlobalContext.java
+++ src/java.base/share/classes/jdk/crac/impl/GlobalContext.java
@@ -5,10 +5,11 @@ import jdk.crac.Resource;
 import sun.security.action.GetPropertyAction;
 
 public class GlobalContext {
+    private static final String GLOBAL_CONTEXT_IMPL_PROP = "jdk.crac.globalContext.impl";
+
     public static Context<Resource> createGlobalContextImpl() {
-        String impl_prop = "jdk.crac.globalContext.impl";
-        String impl_name = GetPropertyAction.privilegedGetProperty(impl_prop, "");
-        return switch (impl_name) {
+        String implName = GetPropertyAction.privilegedGetProperty(GLOBAL_CONTEXT_IMPL_PROP, "");
+        return switch (implName) {
             case "BlockingOrderedContext" -> new BlockingOrderedContext<>();
             case "OrderedContext" -> new OrderedContext<>();
             default -> new OrderedContext<>(); // cannot report as System.out/err are not initialized yet

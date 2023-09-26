e15f9a97868df938ae56455277fc943ccd304527
Radim Vansa <rvansa@azul.com>
2023-09-26 11:34:26 +0200

    Improved classpath matching

--- test/jdk/jdk/crac/fileDescriptors/CheckpointWithOpenFdsTest.java
+++ test/jdk/jdk/crac/fileDescriptors/CheckpointWithOpenFdsTest.java
@@ -25,10 +25,23 @@ import jdk.crac.Core;
 import jdk.test.lib.Utils;
 import jdk.test.lib.crac.CracBuilder;
 import jdk.test.lib.crac.CracTest;
+import jdk.test.lib.crac.CracTestArg;
+
+import java.io.File;
+import java.io.RandomAccessFile;
 import java.nio.file.Path;
 import java.util.*;
+import java.util.stream.Collectors;
+
+import static jdk.test.lib.Asserts.assertEquals;
+import static jdk.test.lib.Asserts.assertTrue;
 
 /**
+ * This test includes two behaviours:
+ * 1) inheriting open FD from parent process: this is achieved using EXTRA_FD_WRAPPER
+ *    - any excess inherited FDs should be closed when JVM starts.
+ * 2) open files on classpath: these files are ignored, handling is left to CREngine
+ *
  * @test
  * @library /test/lib
  * @build CheckpointWithOpenFdsTest
@@ -38,16 +51,41 @@ import java.util.*;
 public class CheckpointWithOpenFdsTest implements CracTest {
     private static final String EXTRA_FD_WRAPPER = Path.of(Utils.TEST_SRC, "extra_fd_wrapper.sh").toString();
 
+    @CracTestArg(optional = true, value = 0)
+    String relativePathToSomeJar;
+
+    @CracTestArg(optional = true, value = 1)
+    String absolutePathToSomeJar;
+
     @Override
     public void test() throws Exception {
+        List<Path> jars = Arrays.stream(System.getProperty("java.class.path").split(File.pathSeparator))
+                .filter(p -> p.endsWith(".jar")).map(Path::of).toList();
+        assertEquals(2, jars.size()); // usually we have javatest.jar and jtreg.jar
+        assertTrue(jars.stream().allMatch(jar -> jar.toFile().exists()));
+        Path firstJar = jars.get(0);
+        Path secondJar = jars.get(1);
+        assertTrue(secondJar.isAbsolute());
+        Path cwd = Path.of(System.getProperty("user.dir"));
+        String relative = cwd.relativize(firstJar).toString();
+        String absolute = secondJar.toString();
+
         CracBuilder builder = new CracBuilder();
+        builder.classpathEntry(relative).classpathEntry(absolute).args(CracTest.args(relative, absolute));
         builder.startCheckpoint(Arrays.asList(EXTRA_FD_WRAPPER, CracBuilder.JAVA)).waitForCheckpointed();
         builder.captureOutput(true).doRestore().outputAnalyzer().shouldContain(RESTORED_MESSAGE);
     }
 
     @Override
     public void exec() throws Exception {
-        Core.checkpointRestore();
-        System.out.println(RESTORED_MESSAGE);
+        String absolute = Path.of(relativePathToSomeJar).toAbsolutePath().toString();
+
+        Path cwd = Path.of(System.getProperty("user.dir"));
+        String relative = cwd.relativize(Path.of(absolutePathToSomeJar)).toString();
+        try (var file1 = new RandomAccessFile(absolute, "r");
+             var file2 = new RandomAccessFile(relative, "r")) {
+            Core.checkpointRestore();
+            System.out.println(RESTORED_MESSAGE);
+        }
     }
 }

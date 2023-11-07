fb1cf056531a0882c4dee69d8e814089c439601f
Radim Vansa <rvansa@azul.com>
2023-11-07 13:17:39 +0100

    Add comment

--- src/hotspot/os/linux/globals_linux.hpp
+++ src/hotspot/os/linux/globals_linux.hpp
@@ -113,6 +113,10 @@ define_pd_global(size_t, PreTouchParallelChunkSize, 4 * M);
 define_pd_global(bool, UseLargePages, false);
 define_pd_global(bool, UseLargePagesIndividualAllocation, false);
 define_pd_global(bool, UseThreadPriorities, true) ;
+
+// On some systems using SSSD files in this directory are left open
+// after calling getpwuid_r, getpwname_r, getgrgid_r, getgrname_r
+// or other functions in this family.
 define_pd_global(ccstrlist, CRAllowedOpenFilePrefixes, "/var/lib/sss/mc/");
 
 #endif // OS_LINUX_GLOBALS_LINUX_HPP

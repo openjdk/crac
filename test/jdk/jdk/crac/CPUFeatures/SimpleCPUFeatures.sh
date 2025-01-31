adc301ed172532696ac7c564b56529ed1e7132e8
Timofei Pushkin <tpushkin@openjdk.org>
2025-01-31 09:23:52 +0100

    Add missing error message ending

--- test/jdk/jdk/crac/CPUFeatures/SimpleCPUFeatures.sh
+++ test/jdk/jdk/crac/CPUFeatures/SimpleCPUFeatures.sh
@@ -5,7 +5,7 @@
 # @run shell SimpleCPUFeatures.sh
 # @summary On old glibc (without INCLUDE_CPU_FEATURE_ACTIVE) one could get an internal error.
 # Error occurred during initialization of VM
-# internal error: GLIBC_TUNABLES=:glibc.cpu.hwcaps=,-AVX,-FMA,-AVX2,-BMI1,-BMI2,-ERMS,-LZCNT,-SSSE3,-POPCNT,-SSE4_1,-SSE4_2,-AVX512F,-AVX512CD,-AVX512BW,-AVX512DQ,-AVX512VL,-IBT,-MOVBE,-SHSTK,-XSAVE,-OSXSAVE,-HTT failed and HOTSPOT_GLIBC_TUNABLES_REEXEC is
+# internal error: GLIBC_TUNABLES=:glibc.cpu.hwcaps=,-AVX,-FMA,-AVX2,-BMI1,-BMI2,-ERMS,-LZCNT,-SSSE3,-POPCNT,-SSE4_1,-SSE4_2,-AVX512F,-AVX512CD,-AVX512BW,-AVX512DQ,-AVX512VL,-IBT,-MOVBE,-SHSTK,-XSAVE,-OSXSAVE,-HTT failed and HOTSPOT_GLIBC_TUNABLES_REEXEC is set
 
 set -ex
 

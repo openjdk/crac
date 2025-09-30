d5f1cc35281ea9d436be1d4c880bc70d016bba86
Radim Vansa <rvansa@azul.com>
2025-09-30 15:36:04 +0200

    Fix build

--- src/hotspot/cpu/aarch64/vm_version_aarch64.hpp
+++ src/hotspot/cpu/aarch64/vm_version_aarch64.hpp
@@ -64,7 +64,9 @@ protected:
 public:
   // Initialization
   static void initialize();
-  struct VM_Features {};
+  struct VM_Features {
+    inline int print_numbers(char *buf_orig, size_t buflen, bool hexonly = false) const { return 0; }
+  };
   static bool cpu_features_binary(VM_Features *data) { return false; }
   static bool cpu_features_binary_check(const VM_Features *data) { return data == nullptr; }
   static bool ignore_cpu_features() { return true; }

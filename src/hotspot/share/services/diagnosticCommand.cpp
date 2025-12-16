87ac427e1db9338c04851a18c96654c38ed17269
Radim Vansa <rvansa@azul.com>
2025-12-16 11:16:49 +0100

    Add missing include

--- src/hotspot/share/services/diagnosticCommand.cpp
+++ src/hotspot/share/services/diagnosticCommand.cpp
@@ -50,6 +50,7 @@
 #include "oops/oop.inline.hpp"
 #include "oops/typeArrayOop.inline.hpp"
 #include "prims/jvmtiAgentList.hpp"
+#include "runtime/crac.hpp"
 #include "runtime/fieldDescriptor.inline.hpp"
 #include "runtime/flags/jvmFlag.hpp"
 #include "runtime/handles.inline.hpp"

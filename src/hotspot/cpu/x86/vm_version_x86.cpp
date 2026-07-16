96e0bab031f6bbbeb838fb7fa6a4f5ae75e546b8
Jan Kratochvil <jkratochvil@azul.com>
2026-07-16 19:47:08 +0200

    Remove unconditional setting of MMX_UNUSED and FLUSH_UNUSED as they are in *_mandatory_()

--- src/hotspot/cpu/x86/vm_version_x86.cpp
+++ src/hotspot/cpu/x86/vm_version_x86.cpp
@@ -3229,9 +3229,6 @@ VM_Version::VM_Features VM_Version::CpuidInfo::feature_flags() const {
       }
     }
   }
-  // Unused fields, see the enum definition.
-  vm_features.set_feature(CPU_MMX_UNUSED);
-  vm_features.set_feature(CPU_FLUSH_UNUSED);
 
   // Composite features.
   if (supports_tscinv_bit() &&

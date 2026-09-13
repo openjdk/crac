6835607b2a16bb182d8f29385e69342a46344f9a
Jan Kratochvil <jkratochvil@azul.com>
2026-09-13 17:37:44 +0200

    Fix an SVE assertion

--- src/hotspot/cpu/aarch64/vm_version_aarch64.cpp
+++ src/hotspot/cpu/aarch64/vm_version_aarch64.cpp
@@ -972,7 +972,7 @@ bool VM_Version::process_image_cpu_features(const VM_Features *image_featuresp) 
     return true;
   }
   if (!_cpu_features.supports_feature(CPU_SVE)) {
-    guarantee(want_sve_vector_length == 16, "CPU_SVE256 cannot be present without CPU_SVE");
+    guarantee(want_sve_vector_length == 0 || want_sve_vector_length == 16, "CPU_SVE256 cannot be present without CPU_SVE");
     return true;
   }
   // Always call PR_SVE_SET_VL, we do not know who did execute this JVM.
